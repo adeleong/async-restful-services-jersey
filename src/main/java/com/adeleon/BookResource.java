1cbd1477926c60abade7d4feae9730558aea1ef5
Aderso De Leon Guzman <aderso.deleon@gmail.com>
2018-11-25 13:19:47 -0400

    Async Resources and Dao Async with Google Guava dependency. Test fail by Moxy WriteMessageBody Generic Type.

--- src/main/java/com/adeleon/BookResource.java
+++ src/main/java/com/adeleon/BookResource.java
@@ -1,6 +1,13 @@
 package com.adeleon;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.glassfish.jersey.server.ManagedAsync;
+
 import javax.ws.rs.*;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
@@ -12,21 +19,52 @@ public class BookResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Book> getBooks(){
-        return (dao.getBooks());
+    @ManagedAsync
+    public void getBooks(@Suspended final AsyncResponse response){
+        response.resume(dao.getBooks());
+        ListenableFuture<Collection<Book>> booksFuture = dao.getBooksAsync();
+        Futures.addCallback(booksFuture, new FutureCallback<Collection<Book>>() {
+            public void onSuccess(Collection<Book> books) {
+                response.resume(books);
+            }
+
+            public void onFailure(Throwable throwable) {
+                response.resume(throwable);
+            }
+        });
     }
 
     @Path("/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Book getBook(@PathParam("id") String id){
-        return dao.getBook(id);
+    @ManagedAsync
+    public void getBook(@PathParam("id") String id, @Suspended final AsyncResponse response){
+        ListenableFuture<Book> bookListenableFuture = dao.getBookAsync(id);
+        Futures.addCallback(bookListenableFuture, new FutureCallback<Book>() {
+            public void onSuccess(Book book) {
+                response.resume(book);
+            }
+
+            public void onFailure(Throwable throwable) {
+                response.resume(throwable);
+            }
+        });
+        response.resume(dao.getBook(id));
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Book addBook(Book book){
-        return (dao.addBook(book));
+    @ManagedAsync
+    public void addBook(Book book, @Suspended final AsyncResponse response){
+        ListenableFuture<Book> bookFuture = dao.addBookAsync(book);
+        Futures.addCallback(bookFuture, new FutureCallback<Book>() {
+            public void onSuccess(Book addedBook) {
+                response.resume(addedBook);
+            }
+            public void onFailure(Throwable throwable) {
+                response.resume(throwable);
+            }
+        });
     }
 }
